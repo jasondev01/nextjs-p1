c68345c6107ff3a00bb55dbe3be91b49e8f2dd6c
jasondev01 <jsnrbn01@gmail.com>
2023-09-08 02:30:44 +0800

    updated the profile component

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,12 +1,18 @@
 import PromptCard from './PromptCard'
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    const capitalizedName = capitalizeFirstLetter(name);
+    
     return (
         <section className='w-full'>
             <h1 className='head_text text-left'>
 
                 <span className='blue_gradient'>
-                    {name != 'My' ? `${name}'s Profile` : `${name} Profile` }
+                    {name != 'My' ? `${capitalizedName}'s Profile` : `${name} Profile` }
                 </span>
             </h1>
             <p className='desc text-left'>
