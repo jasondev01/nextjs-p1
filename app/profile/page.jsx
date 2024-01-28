8f19aa52654834682d1771ca0032a44e83b6ec96
jasondev01 <jsnrbn01@gmail.com>
2024-01-28 22:23:53 +0800

    refactored

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -22,7 +22,7 @@ const MyProfile = () => {
     }, [session?.user.id])
 
     const handleEdit = (post) => {
-        router.push(`/update-prompt?id=${post?._id}`)
+        router.push(`/update-prompt/${post?._id}`)
     }
 
     const handleDelete = async (post) => {
